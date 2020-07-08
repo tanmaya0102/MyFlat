4f5b10a62e83ef0c4779592e41448e4717710b3c
tanmaya0102 <tanmaya0102@gmail.com>
2020-07-08 23:30:54 +0530

    Version Level Commit

--- app/src/main/java/com/tanmaya/myflat/DeveloperActivity.java
+++ app/src/main/java/com/tanmaya/myflat/DeveloperActivity.java
@@ -3,16 +3,22 @@ package com.tanmaya.myflat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class DeveloperActivity extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+    Button LI,GH;
     public DeveloperActivity() {
         // Required empty public constructor
     }
@@ -36,6 +42,46 @@ public class DeveloperActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_developer, container, false);
+        LI=view.findViewById(R.id.button3);
+        GH=view.findViewById(R.id.button5);
+        LI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent("android.intent.action.MAIN");
+                    i.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+                    i.addCategory("android.intent.category.LAUNCHER");
+                    i.setData(Uri.parse("https://www.linkedin.com/in/tanmaya-harichandan-15311513b/"));
+                    startActivity(i);
+                }
+                catch(ActivityNotFoundException e) {
+// Chrome is probably not installed
+// OR not selected as default browser OR if no Browser is selected as default browser
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/tanmaya-harichandan-15311513b/"));
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                }
+            }
+        });
+        GH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent("android.intent.action.MAIN");
+                    i.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+                    i.addCategory("android.intent.category.LAUNCHER");
+                    i.setData(Uri.parse("https://github.com/tanmaya0102"));
+                    startActivity(i);
+                }
+                catch(ActivityNotFoundException e) {
+// Chrome is probably not installed
+// OR not selected as default browser OR if no Browser is selected as default browser
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/tanmaya0102"));
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                }
+            }
+        });
 
         // Inflate the layout for this fragment
         return view;
--- app/src/main/java/com/tanmaya/myflat/fragmenthome.java
+++ app/src/main/java/com/tanmaya/myflat/fragmenthome.java
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class fragmenthome extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -19,6 +24,7 @@ public class fragmenthome extends Fragment {
     private String mParam1;
     private String mParam2;
     ImageView imageView;
+    Button upi,bill,reminder;
     public fragmenthome() {
         // Required empty public constructor
     }
@@ -58,7 +64,41 @@ public class fragmenthome extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-
+        upi=view.findViewById(R.id.button);
+        bill=view.findViewById(R.id.button2);
+        reminder=view.findViewById(R.id.button4);
+        upi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"OOPS! This feature is not available right now",Toast.LENGTH_LONG).show();
+            }
+        });
+        bill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"You will be redirected !",Toast.LENGTH_LONG).show();
+                try {
+                    Intent i = new Intent("android.intent.action.MAIN");
+                    i.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+                    i.addCategory("android.intent.category.LAUNCHER");
+                    i.setData(Uri.parse("https://epayment.dhbvn.org.in/"));
+                    startActivity(i);
+                }
+                catch(ActivityNotFoundException e) {
+// Chrome is probably not installed
+// OR not selected as default browser OR if no Browser is selected as default browser
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://epayment.dhbvn.org.in/"));
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                }
+            }
+        });
+        reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Work in Progress!",Toast.LENGTH_LONG).show();   
+            }
+        });
         // Inflate the layout for this fragment
         return view;
     }
